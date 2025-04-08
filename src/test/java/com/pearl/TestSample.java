6dacba2ea9b991b06ce562cf9181bc8b065776c0
Pearlochani131677 <140430009+Pearlochani131677@users.noreply.github.com>
2025-04-08 10:20:09 +0530

    Create TestSample.java

---
+++ src/test/java/com/pearl/TestSample.java
@@ -0,0 +1,11 @@
+package com.pearl;
+
+import org.junit.Test;
+
+public class TestSample {
+
+    @Test
+    public void testPrint() {
+        System.out.println("Jenkins test ran successfully!");
+    }
+}
